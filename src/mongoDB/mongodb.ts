9643f58070521866202852eca959dd69ba5ae919
Jan <jeligio808@gmail.com>
2021-06-07 00:54:42 -1000

    Connect to MongoDB

---
+++ src/mongoDB/mongodb.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import { MONGO_URI } from '../config/keys';
+
+export async function connectToDatabase () {
+    console.log('Connecting to MongoDB');
+    try {
+        await mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+        console.log('Successfully connected to MongoDB');
+    } catch (err) {
+        console.error.bind(err, 'MongoDB error:');
+    }
+}
